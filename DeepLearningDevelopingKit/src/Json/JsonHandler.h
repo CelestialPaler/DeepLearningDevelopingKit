0e38a6a777ebe536688ab4de4d9e689a0292a5ac
天师苍邪 <sun1106153343@hotmail.com>
2018-04-28 00:19:34 +0800

    JsonHandler in c++ will use RapidJson library.

---
+++ DeepLearningDevelopingKit/src/Json/JsonHandler.h
@@ -0,0 +1,19 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 			  Json Handler 	                                                              */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+#pragma once
+
+#include <iostream>
+
+
+class JsonHandler
+{
+public:
+	JsonHandler();
+
+private:
+
+};
