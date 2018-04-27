4003d9129858efa762b69bc5f82b252fb924fa30
天师苍邪 <sun1106153343@hotmail.com>
2018-04-28 00:31:53 +0800

    Scatch JsonHandler class in pyhton.

--- DeepLearningDevelopingKit/src/Json/JsonHandler.py
+++ DeepLearningDevelopingKit/src/Json/JsonHandler.py
@@ -1,16 +1,20 @@
-﻿#**************************************************************************************************#
+#**************************************************************************************************#
 #                                               Deep Learning Developing Kit                                                    #
 #								        		 	          Json Handler   	                                                           #
 #                                                   www.tianshicangxie.com                                                         #
 #                                      Copyright © 2015-2018 Celestial Tech Inc.                                           #
 #**************************************************************************************************#
 
+# modules
 import os  
 import json
 
-test_json = '.\\test.json'
-
+# Class : JsonHandler
+# Used for handling Json files, loading and writing to Json files.
+# Parsing and dumping data from all kinds of dara sources.
 class JsonHandler:
+
+    # Open a common file. Such as plaintext.
     @staticmethod
     def open_file(str):
         if os.path.exists(str):
@@ -21,7 +25,7 @@ class JsonHandler:
         else:
             raise Exception("Invalid file path!")
             
-
+    # Open a Json file and grab all the data in to a python list.
     @staticmethod
     def open_json(str):
         if os.path.exists(str):
@@ -30,14 +34,16 @@ class JsonHandler:
             file.close()
             return data
 
+    # Dump a list of python object into a Json file in Json format.
     @staticmethod
     def save_json(str):
         pass
 
 
 if "__name__" == "__main__":
+    test_json = '..\\data\\test\\test.json'
     try:
-        data = JsonHandler.open_file(test_json)
+        data = JsonHandler.open_json(test_json)
     except ValueError:
         print("Open file failed!")
         
