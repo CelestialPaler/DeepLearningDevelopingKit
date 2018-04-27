dfd070b800c321e06350aacb55b5489ca3d7a9e7
天师苍邪 <sun1106153343@hotmail.com>
2018-04-27 23:36:46 +0800

    Adding Python utils and Json to the project.

---
+++ DeepLearningDevelopingKit/Json/JsonHandler.py
@@ -0,0 +1,43 @@
+﻿#**************************************************************************************************#
+#                                               Deep Learning Developing Kit                                                    #
+#								        		 	          Json Handler   	                                                           #
+#                                                   www.tianshicangxie.com                                                         #
+#                                      Copyright © 2015-2018 Celestial Tech Inc.                                           #
+#**************************************************************************************************#
+
+import os  
+import json
+
+test_json = '.\\test.json'
+
+
+class JsonHandler:
+    @staticmethod
+    def open_file(str):
+        if os.path.exists(str):
+            with open(str, 'r', encoding='utf-8') as file:
+                data = file.read()
+            file.close()
+            return data
+
+    @staticmethod
+    def open_json(str):
+        if os.path.exists(str):
+            with json.load(str) as file:
+                data = file.read()
+            file.close()
+            return data
+
+    @staticmethod
+    def save_json(str):
+        pass
+
+
+if "__name__" == "__main__":
+    data = JsonHandler.open_file(test_json)
+    print
+    print(data)
+
+
+
+  
