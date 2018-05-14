9a889470be94abe231135fa0c3d13d520a72bb53
天师苍邪 <sun1106153343@hotmail.com>
2018-05-14 23:16:36 +0800

    Updating SVM.

---
+++ DeepLearningDevelopingKit/src/Algorithm/SupportedVectorMachine/SupportedVectorMachine.cpp
@@ -0,0 +1,12 @@
+﻿#include "SupportedVectorMachine.h"
+/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	  Support Vector Machine     	                                               */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+SVM::SVM::SVM()
+{
+
+}
---
+++ DeepLearningDevelopingKit/src/Algorithm/SupportedVectorMachine/SupportedVectorMachine.h
@@ -0,0 +1,19 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	  Support Vector Machine     	                                               */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+namespace SVM
+{
+	class SVM
+	{
+	public:
+
+		SVM();
+
+	private:
+
+	};
+}
