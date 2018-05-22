c89cfcffec52906af5ea954a5cde02a0b828c26c
天师苍邪 <sun1106153343@hotmail.com>
2018-05-22 22:06:53 +0800

    Adding OpenCV for image and video processing and data visualizing.

---
+++ DeepLearningDevelopingKit/src/UnitTest/OpenCV_test.cpp
@@ -0,0 +1,27 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	         OpenCV Test   															  */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+#define OpenCVDebug
+
+#ifdef OpenCVDebug
+
+#include "..\Visualizer\OpenCV\OpenCV.h"
+
+int main(int argc, char ** argv)
+{
+	cv::Mat imageTest = cv::imread("F:\\Software\\Top Peoject\\DeepLearningProject\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\DeepLearningDevelopingKit\\data\\XO\\Data\\O_0.png");
+	if (!imageTest.data)
+		return -1;
+	cv::resize(imageTest, imageTest, cv::Size(100, 100), cv::INTER_LINEAR);
+	cv::imshow("Rem", imageTest);
+	cv::waitKey();
+	system("pause");
+	return 0;
+}
+
+#endif // OpenCVDebug
+
---
+++ DeepLearningDevelopingKit/src/Visualizer/OpenCV/OpenCV.h
@@ -0,0 +1,46 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	         OpenCV Test   															  */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// Header files
+#include <iostream>
+
+// OpenCV
+#include "..\..\..\external\opencv\include\opencv2\core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+
+
+/***************************************************************************************************/
+// Namespace : Visual
+/// Provide tools for data visualization.
+namespace Visual
+{
+	class OpenCV
+	{
+	public:
+
+		OpenCV();
+
+	private:
+
+	};
+
+	class Image
+	{
+	public:
+
+		Image();
+
+	private:
+
+	};
+
+
+}
+
+
